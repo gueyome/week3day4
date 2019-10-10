1b37babff2358a849b6949da8def036a7239c78d
guillaume <gueyome@hotmail.fr>
2019-10-10 19:19:25 +0200

    commit-2

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,7 +4,5 @@ describe "the scrapper still work" do
     it "should return an array not nil" do
       expect(join_my_two_arrays(take_symbol("https://coinmarketcap.com/all/views/all/"),take_value("https://coinmarketcap.com/all/views/all/"))).not_to be_nil
     end
-    it "should contain XRP money" do
-        expect(join_my_two_arrays(take_symbol(url),take_value(url))).
 end
 
